3e67b1a1b97b635a733438dfa27b4fe3e5615972
AliMrad1 <am8396280@gmail.com>
2022-12-16 20:07:36 +0200

    add news to a specific category

--- src/app/newscategory/newscategory-routing.module.ts
+++ src/app/newscategory/newscategory-routing.module.ts
@@ -2,14 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewscategoryComponent } from './newscategory.component';
 
-const routes: Routes = [{ path: 'beauty', component: NewscategoryComponent },
-{ path: 'general', component: NewscategoryComponent },
-{ path: 'business', component: NewscategoryComponent },
-{ path: 'technology', component: NewscategoryComponent },
-{ path: 'sports', component: NewscategoryComponent },
-{ path: 'entrainment', component: NewscategoryComponent },
-{ path: 'health', component: NewscategoryComponent },
-{ path: 'science', component: NewscategoryComponent }
+const routes: Routes = [{ path: ':category', component: NewscategoryComponent }
 ];
 
 @NgModule({
--- src/app/newscategory/newscategory.component.ts
+++ src/app/newscategory/newscategory.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { News } from '../classes/Models/News';
+import { NewsServiceService } from '../classes/service/news-service.service';
 
 @Component({
   selector: 'app-newscategory',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewscategoryComponent implements OnInit {
 
-  constructor() { }
+  public routeCategory:string|any = "";
+  public newsCategory:News[] = [];
+  constructor(private activatedRoute:ActivatedRoute, private http:NewsServiceService) {
+   }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param) => {
+    this.routeCategory = param.get('category');
+  })
+
+  this.http.geNewsToASpecificCategory(this.routeCategory).subscribe((response:any) => {
+    this.newsCategory = response.articles;
+    console.log(this.newsCategory);
+  })
+
+    
   }
 
 }
